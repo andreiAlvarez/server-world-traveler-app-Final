5e59d5295fa1cd0fe3446256ba991e3e41890108
Andrei Alvarez <andreialvarezf@gmail.com>
2020-12-11 12:29:49 -0500

    enable cors

--- app.js
+++ app.js
@@ -29,14 +29,18 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_POINT],
     credentials: true,
-    headers: {
-      'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      }
     // this needs set up on the frontend side as well
     //                   in axios "withCredentials: true"
   })
 );
+
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 // require session
 require('./configs/session.config')(app);
